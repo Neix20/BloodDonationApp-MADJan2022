48dec5cf3924c287fc708227cdf376bd0916dbfa
Neix20 <66442152+Neix20@users.noreply.github.com>
2022-04-01 15:25:52 +0800

    Change Main Drawer

--- App-Project/app/src/main/java/my/edu/utar/blooddonationmad/Admin/ui/MainActivity.java
+++ App-Project/app/src/main/java/my/edu/utar/blooddonationmad/Admin/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package my.edu.utar.blooddonationmad;
+package my.edu.utar.blooddonationmad.Admin.ui;
 
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +7,7 @@ import android.view.Menu;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -14,6 +15,7 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
+import my.edu.utar.blooddonationmad.R;
 import my.edu.utar.blooddonationmad.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,31 +31,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        navigationView.setNavigationItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.nav_logout) {
+                new AlertDialog.Builder(this)
+                        .setMessage("Are you sure you want to logout?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", (dialog, id) -> finish())
+                        .setNegativeButton("No", (dialog, id) -> dialog.cancel())
+                        .show();
+            } else {
+                NavigationUI.onNavDestinationSelected(item, navController);
+                drawer.closeDrawers();
+            }
+            return false;
+        });
     }
 
     @Override
@@ -61,5 +63,15 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", (dialog, id) -> finishAffinity())
+                .setNegativeButton("No", (dialog, id) -> dialog.cancel())
+                .show();
     }
 }
