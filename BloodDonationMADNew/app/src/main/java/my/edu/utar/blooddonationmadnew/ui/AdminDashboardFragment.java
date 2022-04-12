94d0cc7b1fe27de761a4a89839f57b0ca121214a
thisis-JK <ong_junkai000407@hotmail.com>
2022-04-12 21:01:18 +0800

    Completed Topic Listener
    sdfg

--- BloodDonationMADNew/app/src/main/java/my/edu/utar/blooddonationmadnew/ui/AdminDashboardFragment.java
+++ BloodDonationMADNew/app/src/main/java/my/edu/utar/blooddonationmadnew/ui/AdminDashboardFragment.java
@@ -2,6 +2,7 @@ package my.edu.utar.blooddonationmadnew.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,9 +14,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import my.edu.utar.blooddonationmadnew.adapter.NotificationViewAdapter;
 
@@ -57,6 +61,16 @@ public class AdminDashboardFragment extends Fragment {
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
 
         // Subscribe to Push notification
+        FirebaseMessaging.getInstance().subscribeToTopic("admin").addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(!task.isSuccessful()){
+                    Log.e(TAG, "Task has failed!");
+                    return;
+                }
+                Log.i(TAG, "Successfully subscribed to Topic Admin!");
+            }
+        });
 
         fab.setOnClickListener(v -> nAddNotification());
 
